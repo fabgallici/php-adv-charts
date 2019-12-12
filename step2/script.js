27c08eb048acf17208529f839a1b8bdbceee9779
cabal82 <fabriziogallici@gmail.com>
2019-12-12 16:02:03 +0100

    vers agg

--- step2/script.js
+++ step2/script.js
@@ -12,46 +12,15 @@ function getChartData() {
   });
 }
 
+//versione Agents semplice da usare con obj keys e values
 function evData(data) {
   var fatturato = data.fatturato;
-  var fatturato_by_agent = data.fatturato_by_agent;
+  var fatturatoAgents = data.fatturato_by_agent;
   console.log('fatturato ', fatturato);
-  console.log('fatturato_by_agent ', fatturato_by_agent);
+  console.log('fatturatoAgents ', fatturatoAgents);
   printChartFatturato(fatturato);
-  //estrazione dati agent chiave e valori , inserimento stessi in 2 array in nuovo oggetto per stampa video chartjs
-  var fatturatoAgents =
-  {
-    'type': fatturato_by_agent.type,
-    'agent_names': [],
-    'data': []
-  };
-  for (var agentName in fatturato_by_agent.data) {
-    fatturatoAgents.agent_names.push(agentName);
-    fatturatoAgents.data.push(fatturato_by_agent.data[agentName]);
-  }
-  console.log('agents: ', fatturatoAgents);
   printChartFatturatoAgent(fatturatoAgents);
 }
-
-// //alt vers: aggiunge dati oggetto fatturatoAgent attuale senza crearne uno nuovo
-// function evData(data) {
-//   var fatturato = data.fatturato;
-//   var fatturatoAgent = data.fatturato_by_agent;
-//   console.log('fatturato ', fatturato);
-//   console.log('fatturato_by_agent ', fatturatoAgent);
-//   printChartFatturato(fatturato);
-//   //ev dati Agents
-//   fatturatoAgent.agent_names = [];
-//   fatturatoAgent.agents_data = [];
-//   for (var agentName in fatturatoAgent.data) {
-//     fatturatoAgent.agent_names.push(agentName);
-//     fatturatoAgent.agents_data.push(fatturatoAgent.data[agentName]);
-//   }
-//   console.log('agents: ', fatturatoAgent);
-//   //mod in print data: fatturatoAgents.data --> fatturatoAgents.agents_data
-//   printChartFatturatoAgent(fatturatoAgent);  
-// }
-
 
 function printChartFatturato(fatturato) {
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -83,18 +52,16 @@ function printChartFatturato(fatturato) {
     }
   });
 }
-
+//per versione semplice obj keys values
 function printChartFatturatoAgent(fatturatoAgents) {
   var ctx = document.getElementById('myAgentsChart').getContext('2d');
   var myChart = new Chart(ctx, {
     type: fatturatoAgents.type,
     data: {
-      labels: fatturatoAgents.agent_names,
-      backgroundColor: '#000',
+      labels: Object.keys(fatturatoAgents.data),
       datasets: [{
         label: 'Vendite',
-        // data: fatturatoAgents.agents_data,
-        data: fatturatoAgents.data,
+        data: Object.values(fatturatoAgents.data),
         backgroundColor: '#fddb88',
         borderColor: '#f4002a',
         borderWidth: 4
@@ -111,7 +78,78 @@ function printChartFatturatoAgent(fatturatoAgents) {
     }
   });
 }
+
 function init() {
   getChartData();
 }
 $(document).ready(init);
+
+// //base version (richiede printChart sotto)
+// function evData(data) {
+//   var fatturato = data.fatturato;
+//   var fatturato_by_agent = data.fatturato_by_agent;
+//   console.log('fatturato ', fatturato);
+//   console.log('fatturato_by_agent ', fatturato_by_agent);
+//   printChartFatturato(fatturato);
+//   //estrazione dati agent chiave e valori , inserimento stessi in 2 array in nuovo oggetto per stampa video chartjs
+//   var fatturatoAgents =
+//   {
+//     'type': fatturato_by_agent.type,
+//     'agent_names': [],
+//     'data': []
+//   };
+//   for (var agentName in fatturato_by_agent.data) {
+//     fatturatoAgents.agent_names.push(agentName);
+//     fatturatoAgents.data.push(fatturato_by_agent.data[agentName]);
+//   }
+//   console.log('agents: ', fatturatoAgents);
+//   printChartFatturatoAgent(fatturatoAgents);
+// }
+
+// //alt vers: aggiunge dati oggetto fatturatoAgent attuale senza crearne uno nuovo
+// function evData(data) {
+//   var fatturato = data.fatturato;
+//   var fatturatoAgent = data.fatturato_by_agent;
+//   console.log('fatturato ', fatturato);
+//   console.log('fatturato_by_agent ', fatturatoAgent);
+//   printChartFatturato(fatturato);
+//   //ev dati Agents
+//   fatturatoAgent.agent_names = [];
+//   fatturatoAgent.agents_data = [];
+//   for (var agentName in fatturatoAgent.data) {
+//     fatturatoAgent.agent_names.push(agentName);
+//     fatturatoAgent.agents_data.push(fatturatoAgent.data[agentName]);
+//   }
+//   console.log('agents: ', fatturatoAgent);
+//   //mod in print data: fatturatoAgents.data --> fatturatoAgents.agents_data
+//   printChartFatturatoAgent(fatturatoAgent);  
+// }
+
+
+// function printChartFatturatoAgent(fatturatoAgents) {
+//   var ctx = document.getElementById('myAgentsChart').getContext('2d');
+//   var myChart = new Chart(ctx, {
+//     type: fatturatoAgents.type,
+//     data: {
+//       labels: fatturatoAgents.agent_names,
+//       backgroundColor: '#000',
+//       datasets: [{
+//         label: 'Vendite',
+//         // data: fatturatoAgents.agents_data,
+//         data: fatturatoAgents.data,
+//         backgroundColor: '#fddb88',
+//         borderColor: '#f4002a',
+//         borderWidth: 4
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero: true
+//           }
+//         }]
+//       }
+//     }
+//   });
+// }
